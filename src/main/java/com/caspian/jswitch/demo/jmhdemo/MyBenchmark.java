9b5bb39785174420f363a9714f78f31f8f3721c7
malekinejad <malekinejad@caspco.ir>
2019-04-22 16:16:34 +0430

    add mode and time unit annotations

--- src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
+++ src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
@@ -1,10 +1,15 @@
 package com.caspian.jswitch.demo.jmhdemo;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
 
-  @Benchmark
+  @Benchmark @BenchmarkMode(Mode.Throughput) @OutputTimeUnit(TimeUnit.MINUTES)
   public void testMethod() {
     int a = 1;
     int b = 2;
