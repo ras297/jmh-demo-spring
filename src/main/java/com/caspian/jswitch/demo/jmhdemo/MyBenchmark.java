1416fbb106af49b41afd10467f955f3206c23ce8
malekinejad <malekinejad@caspco.ir>
2019-04-22 16:26:03 +0430

    use state avoid constant folding by using state avoid unused code elimination by using a BlackHole

--- src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
+++ src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
@@ -4,15 +4,25 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
+  @State(Scope.Thread)
+  public static class MyState {
+    public int a = 1;
+    public int b = 2;
+  }
 
   @Benchmark @BenchmarkMode(Mode.Throughput) @OutputTimeUnit(TimeUnit.MINUTES)
-  public void testMethod() {
-    int a = 1;
-    int b = 2;
-    int sum = a + b;
+  public void testMethod(MyState state, Blackhole blackhole) {
+    int sum1 = state.a + state.b;
+    int sum2 = state.a + state.a + state.b + state.b;
+
+    blackhole.consume(sum1);
+    blackhole.consume(sum2);
   }
 }
