4d69ccc7c210242815f0edd20e2b4ebdbaaa8c4b
malekinejad <malekinejad@caspco.ir>
2019-04-27 10:38:39 +0430

    add spring

--- src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
+++ src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
@@ -2,17 +2,34 @@ package com.caspian.jswitch.demo.jmhdemo;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
   @State(Scope.Thread)
   public static class MyState {
+    @Setup(Level.Trial)
+    public void doSetup() {
+      System.out.println("Do Setup");
+      if (context == null) {
+        System.out.println("init context");
+        context = new ClassPathXmlApplicationContext(
+            "applicationContext.xml");
+        System.out.println("after init");
+      }
+      System.out.println("Did Setup");
+    }
+
+    public ApplicationContext context;
     public int a = 1;
     public int b = 2;
   }
