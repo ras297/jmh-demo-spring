c00091dcddcabc176fcadb776b77d0c816c1d2f4
malekinejad <malekinejad@caspco.ir>
2019-04-28 09:39:15 +0430

    add main method to run the benchmark

--- src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
+++ src/main/java/com/caspian/jswitch/demo/jmhdemo/MyBenchmark.java
@@ -9,12 +9,28 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
+  public static void main(String... args) throws RunnerException {//just run (not debug) it
+    Options opt = new OptionsBuilder()
+        .include(MyBenchmark.class.getSimpleName())
+        .build();
+    Collection<RunResult> runResults = new Runner(opt).run();
+    for (RunResult runResult : runResults) {
+      System.out.println(runResult);
+    }
+  }
+
   @State(Scope.Thread)
   public static class MyState {
     @Setup(Level.Trial)
